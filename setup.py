89f5028988b47f49874e8bd8af333bc15f18f1d0
Tosi Émeric <emeric254@users.noreply.github.com>
2017-01-19 10:37:03 +0100

    Create setup.py

---
+++ setup.py
@@ -0,0 +1,18 @@
+# -*- coding: utf-8 -*-
+
+try:
+    from setuptools import setup
+except ImportError:
+    from distutils.core import setup
+
+setup(
+    name='Micro-Service Squeleton',
+    version='0.1',
+    packages=[],
+    package_dir={},
+    url='https://github.com/emeric254/microservice-squeleton',
+    license='MIT License',
+    author='Tosi Émeric',
+    author_email='emeric254@users.noreply.github.com',
+    description='Micro-Service Squeleton'
+)
