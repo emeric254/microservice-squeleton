e46212c9a11802a43f08031dd1e0ce57883a834e
toast254 <toast254@users.noreply.github.com>
2017-05-09 16:37:26 +0200

    add cors support !

--- handlers/FakeAPIHandler.py
+++ handlers/FakeAPIHandler.py
@@ -8,6 +8,16 @@ logger = logging.getLogger(__name__)
 
 class FakeAPIHandler(web.RequestHandler):
 
+    def set_default_headers(self):  # to allow CORS
+        self.set_header("Access-Control-Allow-Origin", "*")
+        self.set_header("Access-Control-Allow-Headers", "x-requested-with")
+        self.set_header('Access-Control-Allow-Methods', 'POST, GET, OPTIONS')
+
+    def options(self):  # to allow CORS
+        # no body
+        self.set_status(204)
+        self.finish()
+
     def get(self, path_request):
         if path_request == '' or path_request == '/':
             logger.info('user did not write anything')
