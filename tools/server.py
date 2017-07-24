a97b37489769e7204c202ba0a69112d4fefcf9ca
toast254 <toast254@users.noreply.github.com>
2017-07-24 15:16:56 +0200

    Update server.py

--- tools/server.py
+++ tools/server.py
@@ -6,21 +6,23 @@ from tornado import httpserver, ioloop, netutil, process, web
 logger = logging.getLogger(__name__)
 
 
-def start_http(app: web.Application, http_port: int = 80):
+def start_http(app: web.Application, http_port: int = 80, usefork: bool = False):
     """Create app instance(s) binding a port.
 
     :param app: the app to execute in server instances
     :param http_port: port to bind
+    :param usefork: fork or not to use more than one CPU
     """
     http_socket = netutil.bind_sockets(http_port)  # HTTP socket
-    try:  # try to create threads
-        process.fork_processes(0)  # fork
-    except KeyboardInterrupt:  # except KeyboardInterrupt to "properly" exit
-        ioloop.IOLoop.current().stop()
-        exit(0)
-    except AttributeError:  # OS without fork() support ...
-        logger.warning('Can\' fork, continuing with only one (the main) thread ...')
-        pass  # do nothing and continue without multi-threading
+    if usefork:
+        try:  # try to create threads
+            process.fork_processes(0)  # fork
+        except KeyboardInterrupt:  # except KeyboardInterrupt to "properly" exit
+            ioloop.IOLoop.current().stop()
+            exit(0)
+        except AttributeError:  # OS without fork() support ...
+            logger.warning('Can\' fork, continuing with only one (the main) thread ...')
+            pass  # do nothing and continue without multi-threading
     logger.info('Start an HTTP request handler on port : ' + str(http_port))
     server = httpserver.HTTPServer(app)
     server.add_sockets(http_socket)  # bind http port
